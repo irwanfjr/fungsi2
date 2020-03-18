e64e14ce386ce4ceeb2efd9c6cf19fdf8c774bf3
irwanfjr <44388676+irwanfjr@users.noreply.github.com>
2020-03-18 08:39:16 +0700

    Update to_time_ago.php

--- to_time_ago.php
+++ to_time_ago.php
@@ -13,12 +13,12 @@ function to_time_ago( $time ) {
 	} 
 	
 	$time_rules = array ( 
-				12 * 30 * 24 * 60 * 60 => 'year', 
-				30 * 24 * 60 * 60	 => 'month', 
-				24 * 60 * 60		 => 'day', 
-				60 * 60				 => 'hour', 
-				60					 => 'minute', 
-				1					 => 'second'
+				12 * 30 * 24 * 60 * 60 	=> 'year', 
+				30 * 24 * 60 * 60	=> 'month', 
+				24 * 60 * 60		=> 'day', 
+				60 * 60			=> 'hour', 
+				60			=> 'minute', 
+				1			=> 'second'
 	); 
 
 	foreach( $time_rules as $secs => $str ) { 
